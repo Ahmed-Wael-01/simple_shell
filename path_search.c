ded25ab365c3042f434162e8f9786a517f1583fa
lina <habita.lina@univ-ouargla.dz>
2023-10-18 16:38:10 -0700

    search-path

---
+++ path_search.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * execute - gets env
+ * @cmd: a var
+ * @av: a var
+ *
+ * Description: something
+ * Return: 0
+ */
+
+int execute(char *cmd, char **av)
+{
+	struct stat st;
+	int id = getpid();
+
+	if (stat(cmd, &st) == 0)
+	{
+		fork();
+		if (id == getppid())
+		{
+			execve(cmd, av, environ);
+			exit(98);
+		}
+		wait(NULL);
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * path_search - gets env
+ * @str: a var
+ *
+ * Description: something
+ * Return: 0
+ */
+
+int path_search(char **str)
+{
+	struct stat st;
+	int i = 0;
+	int id;
+	char **path = psplice(_getenv("PATH"));
+	char cmd[100];
+
+	if (str == NULL)
+		return (1);
+	if (stat(str[i], &st) == 0)
+	{
+		id = fork();
+		if (id == 0)
+			execve(str[i], str, environ);
+		wait(NULL);
+		shfree(path);
+		shfree(str);
+		return (0);
+	}
+	else
+	{
+		while (path[i] != NULL)
+		{
+			_strcpy(cmd, path[i]);
+			_strcat(cmd, "/");
+			_strcat(cmd, str[0]);
+			if (execute(cmd, str) == 0)
+			{
+				shfree(path);
+				shfree(str);
+				return (0);
+			}
+			i++;
+		}
+	}
+	perror("./shell");
+	shfree(path);
+	shfree(str);
+	return (1);
+}
